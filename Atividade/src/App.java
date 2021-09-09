980d6d0999dbc91734faf5335d123ce3395c271f
NeoSonoda <nickar34@gmail.com>
2021-09-09 15:20:19 -0300

    incluir produtos,sem validação, write e read .txt

--- Atividade/src/App.java
+++ Atividade/src/App.java
@@ -1,3 +1,14 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 public class App {
 
     public static void limpaTela() {
@@ -6,36 +17,65 @@ public class App {
         System.out.flush();
     }
 
-    public static void cadastrarProduto(){
+    public static void cadastrarProduto() throws IOException {
+        Produto prod = new Produto();
+
+        System.out.println("Digite o nome do produto a ser cadastrado");
+        prod.setLocal(System.console().readLine().intern());
+
+        System.out.println("Digite o tipo do produto a ser cadastrado");
+        prod.setTipo(System.console().readLine().intern());
+
+        System.out.println("Digite a marca do produto a ser cadastrado");
+        prod.setMarca(System.console().readLine().intern());
+
+        System.out.println("Digite a caracteristica do produto a ser cadastrado");
+        prod.setCaracteristicas(System.console().readLine().intern());
+
+        System.out.println("Digite o tamanho do produto a ser cadastrado");
+        prod.setTamanho(Integer.parseInt(System.console().readLine().intern()));
+
+        System.out.println("Digite a cor do produto a ser cadastrado");
+        prod.setCor(Integer.parseInt(System.console().readLine().intern()));
+
+        System.out.println("Digite o valor da etiqueta do produto a ser cadastrado");
+        prod.setValorEtiqueta(Double.parseDouble(System.console().readLine().intern()));
+
+        System.out.println("Digite o preço sugerido do produto a ser cadastrado");
+        prod.setPrecoSugerido(Double.parseDouble(System.console().readLine().intern()));
+
+        System.out.println("Digite o valor pago do produto a ser cadastrado");
+        prod.setValorPago(Double.parseDouble(System.console().readLine().intern()));
+
+        prod.setValorMargem(prod.getValorPago());
+        prod.setCodigo(getUltimoCodigo());
+
+        // Escrever no aquivo texto
+        setProduto(prod);
+    }
+
+    public static void listaProdutos() throws IOException {
+        listaProd();
+    }
+
+    public static void editarProduto() {
 
     }
 
-    public static void listaProdutos(){
-        
-    }
+    public static void deletarProduto() {
 
-    public static void editarProduto(){
-        
-    }
-
-    public static void deletarProduto(){
-        
     }
 
     public static int montaMenu() {
 
         try {
-            
+
             System.out.println("O que deseja fazer?");
-            System.out.println("Digite:" + 
-                                "\n1) Cadastrar produto" + 
-                                "\n2) Ver produto" + 
-                                "\n3) Editar produto" + 
-                                "\n4) Deletar produto" +
-                                "\n5) Sair");
-            
+            System.out.println("Digite:" + "\n1) Cadastrar produto" + "\n2) Ver produto" + "\n3) Editar produto"
+                    + "\n4) Deletar produto" + "\n5) Sair");
+
             String ler = System.console().readLine();
-            
+
             return Integer.parseInt(ler.intern());
 
         } catch (Exception e) {
@@ -47,7 +87,7 @@ public class App {
 
     }
 
-    public static void selecionaOpcao(int opcao){
+    public static void selecionaOpcao(int opcao) throws IOException {
 
         switch (opcao) {
             case 1:
@@ -67,17 +107,18 @@ public class App {
                 limpaTela();
                 editarProduto();
                 break;
-            
+
             case 4:
 
                 limpaTela();
                 deletarProduto();
                 break;
-            
-            default: 
+
+            default:
                 System.out.println("Digite apenas valores validos");
         }
     }
+
     public static void main(String[] args) throws Exception {
         int opcao;
 
@@ -86,10 +127,63 @@ public class App {
             opcao = montaMenu();
             selecionaOpcao(opcao);
 
-        } while (opcao <= 3);
+        } while (opcao <= 4);
 
         System.out.println("Bye o(*￣▽￣*)ブ ");
-        
+
+    }
+
+    public static String getUltimoCodigo() throws IOException {
+        String path = "Registros.txt";
+        try {
+            BufferedReader buffRead = new BufferedReader(new FileReader(path));
+            String linha = "";
+            String ultimaLinha = "";
+            while (true) {
+                if (linha != null) {
+                    ultimaLinha = linha;
+
+                } else
+                    break;
+                linha = buffRead.readLine();
+            }
+            buffRead.close();
+            if (ultimaLinha == null)
+                return "0";
+            return ultimaLinha.split("|")[0];
+        } catch (Exception e) {
+            return "0";
+        }
+    }
+
+    public static void listaProd() throws IOException {
+        String path = "Registros.txt";
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+                System.out.println(linha);
+            } else
+                break;
+            linha = buffRead.readLine();
+        }
+        buffRead.close();
+        limpaTela();;
+    }
+
+    public static void setProduto(Produto prod) throws IOException {
+        var path = "Registros.txt";
+
+        FileWriter fw = new FileWriter(path, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter out = new PrintWriter(bw);
+        // cria o arquivo txt separando as informações do produto por "|"
+        String produtoString = Integer.toHexString(prod.getCodigo()) + "|" + prod.getDataEntrada() + "|"
+                + prod.getLocal() + "|" + prod.getTipo() + "|" + prod.getMarca() + "|" + prod.getCaracteristicas() + "|"
+                + prod.getTamanho() + "|" + prod.getCor() + "|" + Double.toString(prod.getValorEtiqueta()) + "|"
+                + Double.toString(prod.getValorMargem()) + "|" + Double.toString(prod.getPrecoSugerido());
+
+                out.println(produtoString);
+                out.close();
     }
 }
-
--- Atividade/src/Produto.java
+++ Atividade/src/Produto.java
@@ -7,50 +7,50 @@ public class Produto {
     
     private int codigo;
 	public int getCodigo() { return codigo; }
-	public void setCodigo() {}
+	public void setCodigo(String Codigo) {codigo = Integer.parseInt(Codigo)+1;}
 
     private LocalDate dataEntrada;
 	public LocalDate getDataEntrada() { return dataEntrada; }
-	public void setDataEntrada () { LocalDate dataEntrada = LocalDate.now();}
+	public void setDataEntrada (LocalDate dataEntrada) { dataEntrada = LocalDate.now();}
 
     private String local;
 	public String getLocal() { return local; }
-	public void setLocal() {}
+	public void setLocal(String Local) {local = Local;}
 
     private String tipo;
 	public String getTipo() { return tipo; }
-	public void setTipo() {}
+	public void setTipo(String Tipo) {tipo = Tipo;}
 
     private String marca;
 	public String getMarca() { return marca; }
-	public void setMarca() {}
+	public void setMarca(String Marca) { marca = Marca;}
 
     private String caracteristicas;
 	public String getCaracteristicas() { return caracteristicas; }
-	public void setCaracteristicas() {}
+	public void setCaracteristicas(String Caracteristicas) {caracteristicas=Caracteristicas;}
 
     private int tamanho;
 	public int getTamanho() { return tamanho; }
-	public void setTamanho() {}
+	public void setTamanho(int Tamanho) {tamanho=Tamanho;}
     
     private int cor;
 	public int getCor() { return cor; }
-	public void setCor() {}
+	public void setCor(int Cor) {cor = Cor;}
 
     private Double valorEtiqueta;
 	public Double getValorEtiqueta() { return valorEtiqueta; }
-	public void setValorEtiqueta() {}
+	public void setValorEtiqueta(Double ValorEtiqueta) {valorEtiqueta=ValorEtiqueta;}
 
     private Double valorPago;
 	public Double getValorPago() { return valorPago; }
-	public void setValorPago() {}
+	public void setValorPago(Double ValorPago) {valorPago=ValorPago;}
     
     private Double valorMargem;
 	public Double getValorMargem() { return valorMargem; }
-	public void setValorMargem(Double valorPago, Double valorMargem) { valorMargem = valorPago * 2; }
+	public void setValorMargem(Double ValorPago) { valorMargem = ValorPago * 2; }
 
     private Double precoSugerido;
 	public Double getPrecoSugerido() { return precoSugerido; }
-	public void setPrecoSugerido() {}
+	public void setPrecoSugerido(Double Preco) {precoSugerido=Preco;}
 
 }
